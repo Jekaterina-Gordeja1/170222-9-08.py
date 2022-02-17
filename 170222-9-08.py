ce2429528f7f6dfee227ddcd36f9bc82350b6bdc
Jekaterina-Gordeja1 <78867046+Jekaterina-Gordeja1@users.noreply.github.com>
2022-02-17 15:32:34 +0200

    Update 170222-9-08.py

--- 170222-9-08.py
+++ 170222-9-08.py
@@ -1,3 +1,17 @@
+i  = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+res = []
+for i in i:
+   res.append(pow(i, 3))
+  
+  
+print(res)
+
+i  = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+res = []
 for i in i:
    res.append(pow(i, 2))
   
