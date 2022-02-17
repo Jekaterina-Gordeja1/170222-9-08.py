a3a075be273d02abafceb7ace8334e50694603e8
Jekaterina-Gordeja1 <78867046+Jekaterina-Gordeja1@users.noreply.github.com>
2022-02-17 15:05:34 +0200

    Create 170222-9-08.py

---
+++ 170222-9-08.py
@@ -0,0 +1,16 @@
+for i in i:
+   res.append(pow(i, 2))
+  
+  
+print(res)
+
+import math
+i = [ 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+
+res = []
+for i in i:
+   res.append(math.sqrt(2))
+  
+  
+print(res)
